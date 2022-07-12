af0ee9a4907235491c6fbfa42b52bb8fd87bfcea
kamal1182 <mkamal1182@gmail.com>
2022-07-12 15:08:17 +0200

    update

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -5,7 +5,7 @@ cd ./www
 echo "ls ./www"
 echo $(ls)
 echo "eb init $EB_APP --region us-east-1  --platform node.js"
-eb init $EB_APP --region us-east-1  --platform node.js
+# eb init $EB_APP --region us-east-1  --platform node.js
 echo "cd ./www/.elasticbeanstalk"
 echo $(cd ./www/.elasticbeanstalk)
 echo "ls ./www/.elasticbeanstalk"
@@ -17,4 +17,5 @@ echo "$EB_ENV"
 eb use $EB_ENV
 echo "eb deploy"
 eb deploy
+echo "eb setenv"
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PORT=$POSTGRES_PORT POSTGRES_DB=$POSTGRES_DB AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE JWT_SECRET=$JWT_SECRET AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
