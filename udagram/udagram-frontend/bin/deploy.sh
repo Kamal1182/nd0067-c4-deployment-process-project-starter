5305f4e1179af345b28c20f096d33c8fecdf41b4
kamal1182 <mkamal1182@gmail.com>
2022-07-13 14:46:32 +0200

    update front

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,2 @@
-npm run build
-aws s3 cp --recursive --acl public-read ./www s3://udagram-ui/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-ui/
+aws s3 cp --recursive --acl public-read ./www s3://udagram-frontend-ui/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-frontend-ui/
